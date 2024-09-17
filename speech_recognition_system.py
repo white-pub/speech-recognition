ca59ee0eda5b2e9ed89548c1241343eef36ed922
white-pub <s96024n@gmail.com>
2024-09-17 15:55:22 -0500

    update

---
+++ speech_recognition_system.py
@@ -0,0 +1,16 @@
+
+# Speech Recognition System
+# Anna Chen
+# 9/16/2024
+#
+# This is a speech recognition system that utilizes the 
+# SpeechRecognition and PyAudiowith library.
+
+
+
+
+
+
+
+# Import needed library
+import speech_recognition as sr
